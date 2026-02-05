4d63798d32777af1d16f7210eac867ff8ae1aff9
Buse Ayvaz <47761924+buseayvaz@users.noreply.github.com>
2026-02-05 12:49:34 +0300

    eklendi

---
+++ scripts/objRefTable.js
@@ -0,0 +1,47 @@
+const C3 = self.C3;
+self.C3_GetObjectRefTable = function () {
+	return [
+		C3.Plugins.Sprite,
+		C3.Plugins.Touch,
+		C3.Plugins.Text,
+		C3.Plugins.Touch.Cnds.IsInTouch,
+		C3.Plugins.System.Cnds.Compare,
+		C3.Plugins.Touch.Exps.TouchCount,
+		C3.Plugins.System.Acts.SetVar,
+		C3.Plugins.System.Acts.SetBoolVar,
+		C3.Plugins.Touch.Cnds.OnDoubleTapGestureObject,
+		C3.Plugins.System.Cnds.PickNth,
+		C3.Plugins.Sprite.Acts.SetBoolInstanceVar,
+		C3.Plugins.System.Acts.CreateObject,
+		C3.Plugins.Touch.Exps.X,
+		C3.Plugins.Touch.Exps.Y,
+		C3.Plugins.System.Acts.AddVar,
+		C3.Plugins.Text.Acts.SetText,
+		C3.Plugins.Text.Acts.SetFontSize,
+		C3.Plugins.Text.Acts.SetFontColor
+	];
+};
+self.C3_JsPropNameTable = [
+	{Map: 0},
+	{IsMarked: 0},
+	{Değişken1: 0},
+	{Target: 0},
+	{Marked: 0},
+	{Dokun: 0},
+	{ScoreText: 0},
+	{IsDragging: 0},
+	{DeltaX: 0},
+	{DeltaY: 0},
+	{TouchLastX: 0},
+	{TouchLastY: 0},
+	{Puan: 0},
+	{Score: 0}
+];
+
+self.InstanceType = {
+	Map: class extends self.ISpriteInstance {},
+	Target: class extends self.ISpriteInstance {},
+	Marked: class extends self.ISpriteInstance {},
+	Dokun: class extends self.IInstance {},
+	ScoreText: class extends self.ITextInstance {}
+}
